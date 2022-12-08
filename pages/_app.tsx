b93253643eb9afd96ab5743cc3a1ca66a077076e
gcunruh <49214225+gcunruh@users.noreply.github.com>
2022-12-08 14:54:57 -0600

    updated wallet adapter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,36 @@
 import React, { FC, useMemo } from "react";
 import type { AppProps } from "next/app";
-import dynamic from "next/dynamic";
-import { ConnectionProvider } from "@solana/wallet-adapter-react";
 import { Toaster } from 'react-hot-toast';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 
+// Default styles that can be overridden by your app
+require('@solana/wallet-adapter-react-ui/styles.css');
 import '../styles/globals.css'
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 require('@solana/wallet-adapter-react-ui/styles.css')
 
-const endpoint = String(process.env.NEXT_PUBLIC_RPC_ENDPOINT);
-
-const WalletProvider = dynamic(
-  () => import("../contexts/ClientWalletProvider"),
-  {
-    ssr: false,
-  }
-)
-
 function MyApp({ Component, pageProps }: AppProps) {
+  const network = String(process.env.NEXT_PUBLIC_CLUSTER)
+  const endpoint = useMemo(() => String(process.env.NEXT_PUBLIC_RPC_ENDPOINT), [network]);
+
+  const wallets = useMemo(
+      () => [
+        new PhantomWalletAdapter()
+      ],
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [network]
+  );
 
   return (
     <>
     <Toaster />
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider>
-          <Component {...pageProps} />
+      <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Component {...pageProps} />
+          </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
     </>
